99db26614597c67e4d05aedc8f635129a0a08b48
iaruna <arunakumariofficial@gmail.com>
2023-03-20 19:26:36 +0530

    optimize

--- app.py
+++ app.py
@@ -13,22 +13,26 @@ def home():
     intro = "Knowledge of Python web frameworks and Basic understanding of front-end technologies"
     return render_template('index.html', name=name, position=position, intro=intro)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', data=data)
+
 
 @app.route('/skills')
 def skills():
     return render_template('skills.html')
 
+
 @app.route('/education')
 def education():
     return render_template("education.html")
+
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
